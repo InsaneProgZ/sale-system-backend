613735924ea475e75028aecaa7f3ca85f97d5356
Yan Zingra <yan.zingra12@gmail.com>
2023-06-26 13:06:15 -0300

    UpdateQuery refactored to create based on passed values

--- src/repository/ProductRepository.go
+++ src/repository/ProductRepository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"database/sql"
+	"log"
+	"reflect"
 	"sale-system/src/model/domain"
 	"time"
 )
@@ -9,8 +11,8 @@ import (
 type Database interface {
 	Save(product domain.Product) (int64, error)
 	FindAll() ([]domain.Product, error)
-	FindByCode(id int64) (domain.Product, error)
-	ChangeProductByCode(id int64, product domain.Product) error
+	FindByCode(code int64) (domain.Product, error)
+	ChangeProductByCode(code int64, product domain.Product) error
 }
 
 type MysqlDB struct {
@@ -58,33 +60,47 @@ func (database *MysqlDB) FindByCode(id int64) (product domain.Product, err error
 	return
 }
 
-func (database *MysqlDB) ChangeProductByCode(id int64, newProduct domain.Product) (err error) {
+func (database *MysqlDB) ChangeProductByCode(code int64, newProduct domain.Product) (err error) {
 
-	_, err = database.FindByCode(id)
+	_, err = database.FindByCode(code)
 
 	if err != nil {
 		return
 	}
 
-	sql := `UPDATE products SET name = ?, buy_price = ?, sell_price = ?, brand = ? WHERE code = ?`
+	sql, params := createUpdateQuery(code, newProduct)
+
 	_, err = database.Mysql.Exec(
 		sql,
-		newProduct.Name,
-		newProduct.BuyPrice,
-		newProduct.SellPrice,
-		newProduct.Brand,
-		id)
+		params...)
 	return
 }
 
-// func createUpdateQuery(product domain.Product) (sql string) {
-// 	sql = `UPDATE into products SET name = ?, buy_price = ?, sell_price = ?, brand = ? WHERE code = ?;`
+func createUpdateQuery(code int64, product domain.Product) (sql string, params []interface{}) {
+	sql = `UPDATE products SET`
 
-// 	for _, field := range product {
-// 		switch field {
-// 		case "Brand":
-// 			sql = strings.Replace(sql, "SET", "SET "+ field + "= ?", 1)
-// 		}
-// 	}
+	productType := reflect.TypeOf(product)
+	productValue := reflect.ValueOf(product)
 
-// }
+	for i := 0; i < productType.NumField(); i++ {
+		log.Println(sql)
+		switch productType.Field(i).Name {
+		case "Name":
+			sql += " name = ?,"
+			params = append(params, productValue.Field(i).Interface())
+		case "BuyPrice":
+			sql += " buy_price = ?,"
+			params = append(params, productValue.Field(i).Interface())
+		case "SellPrice":
+			sql += " sell_price = ?,"
+			params = append(params, productValue.Field(i).Interface())
+		case "Brand":
+			sql += " brand = ?,"
+			params = append(params, productValue.Field(i).Interface())
+		}
+	}
+	sql = sql[:len(sql)-1]
+	sql += " WHERE code = ?"
+	params = append(params, code)
+	return
+}
